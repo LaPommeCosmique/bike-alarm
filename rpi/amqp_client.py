ea98be5e22282b33fa5b8c926be61a5a2a29d3c7
LaPommeCosmique <ayg2001@hotmail.com>
2025-11-25 13:28:11 -0500

    Copy mqtt, http, amqp clients from labs manual

---
+++ rpi/amqp_client.py
@@ -0,0 +1,42 @@
+import pika
+import logging
+# logging.getLogger("pika").setLevel("WARNING")
+
+def connect_to_broker(ip, port, user, passw):
+	credentials = pika.PlainCredentials(user, passw)
+	parameters = pika.ConnectionParameters(ip, port, "/", credentials)
+	connection = pika.BlockingConnection(parameters)
+	channel = connection.channel()
+	logging.info("AMQP - Connected to broker: " + str(ip) + ":" + str(port))
+	return channel
+
+
+def create_queue(channel, exchange, routing_key, queue, durable=False):
+	channel.queue_declare(queue=queue, durable=durable)
+	channel.queue_bind(exchange=exchange, queue=queue, routing_key=routing_key)
+	logging.debug("AMQP - Queue declare and bind: " + queue)
+
+
+def on_message(channel, method, properties, msg):
+	logging.debug("AMQP - Message received: " + msg.decode("utf-8"))
+
+def subscribe(channel, queue, on_message_callback):
+	channel.basic_consume(queue, on_message_callback=on_message_callback)
+	logging.info("AMQP - Subscribed to: " + queue)
+
+
+if __name__ == "__main__":
+	logging.basicConfig(format="%(asctime)s | %(name)s | %(levelname)s | %(message)s", level=logging.DEBUG,)
+	ip = "<rabbitmq ip address>"
+	port = 30672
+	user = "<rabbitmq username>"
+	passw = "<rabbitmq password>"
+	queue = "queue_test"
+	exchange = "amq.topic"
+	routing_key = "routing_key_test"
+	message = "message_test"
+	channel = connect_to_broker(ip, port, user, passw)
+	create_queue(channel, exchange, routing_key, queue)
+	channel.basic_publish(exchange=exchange, routing_key=routing_key, body=message)
+	subscribe(channel, queue, on_message)
+	channel.start_consuming()
---
+++ rpi/http_client.py
@@ -0,0 +1,45 @@
+import requests, json, time
+import logging
+
+def query(url, method, payload=None):
+	try:
+		res = requests.Response()
+		if method == "GET":
+			res = requests.get(url)
+		if method == "POST":
+			res = requests.post(
+				url, 
+				data=json.dumps(payload),
+				headers={"content-type": "application/json"},
+			)
+		if method == "PUT":
+			res = requests.put(
+				url,
+				data=json.dumps(payload),
+				headers={"content-type": "application/json"},
+			)
+		if method == "DELETE":
+			res = requests.delete(url)
+		if res.content is not None:
+			logging.debug("HTTP- [" + method + "][" + str(res.status_code) + "][" + url + "]")
+		return json.loads(res.content.decode("utf8"))
+	except:
+		logging.error("HTTP- " + method + "- Connection error!")
+		
+if __name__ == "__main__":
+	logging.basicConfig(format="%(asctime)s | %(name)s | %(levelname)s | %(message)s", level=logging.DEBUG,)
+	root_url = "http://<elastic url>:32200/"
+	index_es = "testindex"
+	query(
+		url=root_url + index_es + "/test_sensor/1",
+		method="POST",
+		payload={"tester": "workstation", "sensor_data": 23},
+	)
+	query(
+		url=root_url + index_es + "/test_sensor/1",
+		method="PUT",
+		payload={"tester": "workstation", "sensor_data": 15},
+	)
+	time.sleep(1)
+	query(url=root_url + index_es + "/" + "_search", method="GET")
+	query(url=root_url + index_es, method="DELETE")
