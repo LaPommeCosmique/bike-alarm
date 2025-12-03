956b4e4cbeac3843d4bfaf08a2d2cde6912bdd41
jesuarezm <jesuarezm@outlook.com>
2025-12-03 17:55:03 -0500

    Update actuator_controller.py

--- rpi/actuator_controller.py
+++ rpi/actuator_controller.py
@@ -31,9 +31,9 @@ def on_tamper_message(channel, method, properties, msg):
         msg = json.loads(str(msg.decode("utf-8")))
         if msg is not None:
             if msg["tamper"]:
-                mqttc.publish(buzzer_topic, json.dumps({'buzzer':True}))
+                mqttc.publish(buzzer_topic, json.dumps({'buzzer':'tamper'}))
             else:
-                mqttc.publish(buzzer_topic, json.dumps({'buzzer':False}))
+                mqttc.publish(buzzer_topic, json.dumps({'buzzer':'off'}))
     except Exception as e:
         logging.warning(f'{e}')
 
@@ -43,9 +43,9 @@ def on_compromised_message(channel, method, properties, msg):
         msg = json.loads(str(msg.decode("utf-8")))
         if msg is not None:
             if msg["compromised"]:
-                mqttc.publish(buzzer_topic, json.dumps({'buzzer':True}))
+                mqttc.publish(buzzer_topic, json.dumps({'buzzer':'compromised'}))
             else:
-                mqttc.publish(buzzer_topic, json.dumps({'buzzer':False}))
+                mqttc.publish(buzzer_topic, json.dumps({'buzzer':'off'}))
     except Exception as e:
         logging.warning(f'{e}')
 
@@ -54,10 +54,10 @@ def on_compromised_message(channel, method, properties, msg):
 # index_elastic = "temperature"
 
 ## AMQP CONNECTION PARAMETERS
-ip_rabbit = "<rabbitmq ip address>"
+ip_rabbit = "172.20.10.5"
 port_rabbit = 30672
-user_rabbit = "<rabbitmq username>"
-pass_rabbit = "<rabbitmq password>"
+user_rabbit = "user"
+pass_rabbit = "password"
 
 ## AMQP SUBSCRIBER QUEUES
 lock_status_r_key_rabbit = "lock_status" # maybe
@@ -70,14 +70,14 @@ compromised_status_queue_rabbit = "compromised_status"
 ## CONNECT TO AMQP SERVER
 amqp_client.message = None
 amqp_channel = amqp_client.connect_to_broker(ip_rabbit, port=port_rabbit, user=user_rabbit, passw=pass_rabbit)
-amqp_client.subscribe(amqp_channel, lock_status_queue_rabbit, lock_status_r_key_rabbit)
-amqp_client.subscribe(amqp_channel, tamper_status_queue_rabbit, tamper_status_r_key_rabbit)
-amqp_client.subscribe(amqp_channel, compromised_status_queue_rabbit, compromised_status_r_key_rabbit)
+amqp_client.subscribe(amqp_channel, lock_status_queue_rabbit, on_lock_status_message)
+amqp_client.subscribe(amqp_channel, tamper_status_queue_rabbit, on_tamper_message)
+amqp_client.subscribe(amqp_channel, compromised_status_queue_rabbit, on_compromised_message)
 
 ## MQTT CONNECTION PARAMETERS
-ip_mosquitto = "localhost"
+ip_mosquitto = "172.20.10.3"
 lock_status_led_topic = "lock_status_led_topic"
-buzzer_topic = "buzzer_topic_topic"
+buzzer_topic = "buzzer_topic"
 mqttc = mqtt_client.connect(ip_mosquitto)
 
 try:
